cc59f7208873adb09654c77dc770302cd393bda1
Eddie <eddielacrosse2@gmail.com>
2021-12-06 16:45:13 -0500

    Added landing page mobile top bar scroll animation; Added topbar to Profile Page

--- src/app/pages/landing/landing.page.ts
+++ src/app/pages/landing/landing.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ProductsService } from 'src/app/services/products/products.service';
 
 
@@ -10,10 +11,31 @@ import { ProductsService } from 'src/app/services/products/products.service';
 export class LandingPage implements OnInit {
 
   constructor(
-    private productsService: ProductsService
+    private productsService: ProductsService,
+    private router: Router
   ) { }
 
   ngOnInit() {
+    let topBar = document.getElementById('top-bar-logo');
+  }
+  scrollPosition(e) {
+    let scrollTop = e.detail.scrollTop;
+
+    // When user has scrolled passed Header
+    if(scrollTop > 330) {
+      let toolbarBackground = document.getElementById('top-bar');
+      toolbarBackground.style.background = "#fff"
+    } 
+
+    // When user has scrolled back to Header
+    else {
+      let toolbarBackground = document.getElementById('top-bar');
+      toolbarBackground.style.background = "none"
+    }
+    // console.log('Scroll Position: ' + scrollTop)
+  }
+  login() {
+    this.router.navigateByUrl('/login')
   }
   getLatestProducts() {
 
